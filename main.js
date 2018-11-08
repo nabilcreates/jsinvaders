9c5c757514443edc48405ec2a280d3e0bc91a2d9
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-11-08 12:17:09 +0800

    commenting and also enemy is now square

--- main.js
+++ main.js
@@ -26,32 +26,47 @@ function setup() {
 function draw() {
     background(0)
 
-    enemy.y += 1
+    // enemy.y = speed of the enemy  coming down
+    enemy.y += 5
 
+    // set gamecursor config
     gameCursor = {
         x: mouseX,
         y: 500,
         d: 10
     }
 
+    // bullet.y = speed of the bullet
     bullet.x = mouseX
     bullet.y -= 20
 
+    // draw bullet and enemy
     ellipse(bullet.x, bullet.y, bullet.d)
-    ellipse(enemy.x, enemy.y, enemy.d)
+    rect(enemy.x, enemy.y, enemy.d,enemy.d)
 
+    // draw the game cursor
     ellipse(gameCursor.x, gameCursor.y, gameCursor.d)
 
+    // if pressed mouse on canvas, shoot
     cnv.mousePressed(() => {
         shoot()
     })
 
+    // intersect check between bullet and enemy
     if (checkForIntersect(bullet, enemy)) {
         spawn()
         score ++;
     }
 
+    // intersect check between game cursor and the enemy
     if (checkForIntersect(gameCursor, enemy)) {
+        spawn()
+        gameOver()
+    }
+
+    // Check if enemy goes after the width or the height
+    if(enemy.x > width || enemy.y > height){
+        spawn()
         gameOver()
     }
 
@@ -60,7 +75,7 @@ function draw() {
 
 // CHECK FOR INTERSECTION
 function checkForIntersect(itemCheck0, itemCheck1) {
-    return dist(itemCheck0.x, itemCheck0.y, itemCheck1.x, itemCheck1.y) < itemCheck1.d / 2
+    return dist(itemCheck0.x, itemCheck0.y, itemCheck1.x, itemCheck1.y) < itemCheck1.d
 }
 
 // CLICKEVENT (RETURN E TO ORIGINAL SPOT)
